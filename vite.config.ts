5da8660defa02f173090000118a75140a97e88f2
张争 <2834198667@qq.com>
2022-09-11 18:21:14 +0800

    修改打包路径

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import styleImport, { VantResolve } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/template/',
+  base: '/',
   plugins: [
     vue(),
     styleImport({
