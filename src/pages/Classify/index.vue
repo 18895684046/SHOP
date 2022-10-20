7e700cc2fd39ae5cdc3083b6b8f38d722de64d15
V_张争 <v_zhangzheng01@bilibili.com>
2022-10-20 10:04:33 +0800

    chore: 添加分类

--- src/pages/Classify/index.vue
+++ src/pages/Classify/index.vue
@@ -5,38 +5,72 @@
     <div class="sidebar-wrap">
         <div class="left-wrap">
             <van-sidebar v-model="active">
-                <van-sidebar-item @click="onSiderBarChange(c?.name)"  v-for="c in categoryList" :title="c?.name"></van-sidebar-item>
+                <van-sidebar-item @click="onSiderBarChange(c?._id,c?.name)" v-for="c in categoryList" :title="c?.name">
+                </van-sidebar-item>
             </van-sidebar>
         </div>
         <div class="right-wrap">
             <div class="big-title">{{curName}}</div>
+            <div class="level2-wrap">
+                <div class="scroll-content">
+                    <div v-for="c in curLevel2Info" class="single-classify">
+                        <img :src="c?.imageUrl" alt="">
+                        <span>{{c?.keyword}}</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <TabbarCom />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { nextTick, onMounted, reactive, ref, watchEffect } from 'vue'
 import TabbarCom from '@/components/Tabbar.vue'
 import TitleCpm from '@/components/Title.vue';
+import BScroll from 'better-scroll'
 import { getCategoryFunc } from './service/index'
-import { Category } from './interface'
+import { Category, Level2 } from './interface'
 
 const active = ref<number>(0);
 const curName = ref<string>('热门推荐')
 const categoryList = ref<Category[]>([])
+const curLevel2Info = ref<Level2[]>([])
+let bscroll = reactive<any>({})
+
+onMounted(() => {
+    bscroll = new BScroll(document.querySelector('.level2-wrap') as HTMLElement, {
+        probeType: 2,
+        click: true,
+        pullUpLoad: true
+    })
+    bscroll.on('pullingUp',()=>{
+        console.log('11');
+        bscroll.refresh()
+    })
+})
+watchEffect(()=>{
+    nextTick(()=>{
+        console.log('3');
+        
+        // 重新计算高度
+        bscroll && bscroll.refresh()
+    })
+})
 
 getCategoryFunc().then(res => {
     console.log(res, '分类数据');
     if (res?.success) {
         categoryList.value = res?.data
+        curLevel2Info.value = categoryList.value[0]?.level2words
     }
 })
 
 // tabber bar 切换事件
-const onSiderBarChange = (name: string) => {
-    console.log(name, '索引');
+const onSiderBarChange = (id: string, name: string) => {
     curName.value = name
+    const level2Info = categoryList.value?.filter(cate => cate?._id === id)[0]?.level2words
+    curLevel2Info.value = level2Info
 }
 
 </script>
@@ -63,17 +97,47 @@ const onSiderBarChange = (name: string) => {
     }
 
     .right-wrap {
-    
+
         width: calc(100vw - 80px);
         position: fixed;
         top: 45px;
         left: 80px;
-        .big-title{
+
+        .big-title {
             font-size: 15px;
             padding-top: 12px;
             padding-left: 5px;
             font-weight: bold;
         }
+
+        .level2-wrap {
+            height: 100vh;
+            position: absolute;
+            top: 45px;
+            bottom: 50px;
+            left: 0px;
+            right: 0px;
+            overflow: hidden;
+            .scroll-content{
+                padding-bottom: 150px;
+                display: flex;
+                flex-wrap: wrap;
+                background-color: #ffffff;
+                .single-classify{
+                    display: flex;
+                    flex-direction: column;
+                    width: 32.8%;
+                    align-items: center;
+                    justify-content: center;
+                    margin-bottom: 10px;
+                    img{
+                        width: 70px;
+                        height: 70px;
+                    }
+                }
+            }
+        }
+
     }
 }
 </style>
--- src/pages/Classify/interface.ts
+++ src/pages/Classify/interface.ts
@@ -1,8 +1,17 @@
+export type Level2 = {
+    imageUrl: string,
+    keyword: string,
+    keywordId: string,
+    ptag: string,
+}
+
 export type Category = {
-    _id:string,
+    _id: string,
     cid: string,
     commonUsed: string,
     ptag: string,
     level: number,
     name: string,
+    level2words: Level2[]
 }
+
